fe4699db952079711987a7f7fdd42a5a526ba364
Eleni Kapsali <63153771+el2kaps@users.noreply.github.com>
2021-05-28 23:54:47 +0300

    Authentication-tested-only src

---
+++ src/users/users.controller.ts
@@ -0,0 +1,47 @@
+import { UsersService } from './users.service';
+
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { UserI, UserI2 } from "./models/users.interface";
+import { CreateUserDto } from './models/dto/CreateUser.dto';
+import { LoginUserDto } from './models/dto/LoginUser.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post('register')
+  create(@Body() createdUserDto: CreateUserDto): Observable<UserI> {
+    return this.userService.create(createdUserDto);
+  }
+
+  @Post('login')
+  @HttpCode(200)
+  login(@Body() loginUserDto: LoginUserDto): Observable<Object> {
+    return this.userService.login(loginUserDto).pipe(
+      map((jwt: string) => {
+        return {
+          access_token: jwt,
+          token_type: 'JWT',
+          expires_in: 10000,
+        };
+      }),
+    );
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  findAll(@Req() request): Observable<UserI2[]> {
+    return this.userService.findAll();
+  }
+}
