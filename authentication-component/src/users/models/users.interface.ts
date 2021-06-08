df4ff25c489e428f97e9a1c1519f80f2f7d1741f
Eleni Kapsali <63153771+el2kaps@users.noreply.github.com>
2021-06-08 22:13:21 +0300

    Authentication component - Last edition

---
+++ authentication-component/src/users/models/users.interface.ts
@@ -0,0 +1,12 @@
+export interface UserI {
+  UserID: number;
+  name: string;
+  email: string;
+  password?: string;
+}
+
+export interface UserI2 {
+  UserID: number;
+  name: string;
+  email: string;
+}
