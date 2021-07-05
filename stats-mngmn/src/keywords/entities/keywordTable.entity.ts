1af698e84d24523231118171be60f83f1b17cbb3
Eleni Kapsali <63153771+el2kaps@users.noreply.github.com>
2021-07-05 17:44:01 +0300

    Delete stats-mngmn/src directory

--- stats-mngmn/src/keywords/entities/keywordTable.entity.ts
+++
@@ -1,8 +0,0 @@
-import {Entity, JoinTable, ManyToMany, PrimaryColumn, PrimaryGeneratedColumn} from 'typeorm';
-import { Question } from '../../questions/entities/question.entity';
-
-@Entity()
-export class KeywordTable {
-    @PrimaryColumn()
-    keyword: string;
-}
