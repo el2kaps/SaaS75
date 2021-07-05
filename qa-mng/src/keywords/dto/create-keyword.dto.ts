f1de795d3e2acdaa2c2764795aa5f1f141019284
Eleni Kapsali <63153771+el2kaps@users.noreply.github.com>
2021-07-05 17:42:51 +0300

    Delete qa-mng/src directory

--- qa-mng/src/keywords/dto/create-keyword.dto.ts
+++
@@ -1,9 +0,0 @@
-import {IsString, IsDate, IsNumber, IsObject} from 'class-validator';
-import {User} from "../../user/entities/user.entity";
-import {Question} from "../../questions/entities/question.entity";
-export class CreateKeywordDto {
-  @IsString()
-  keyword: string;
-  @IsNumber()
-  Q_ID: number;
-}
