ecbd64fab180754e67b4271d9894eaf68d12952d
Eleni Kapsali <63153771+el2kaps@users.noreply.github.com>
2021-07-05 17:42:19 +0300

    Delete firstsoa directory

--- firstsoa/data-layer/dto/create-question.dto.ts
+++
@@ -1,7 +0,0 @@
-export class Question {
-  readonly Q_ID: number;
-  readonly title: string;
-  readonly text: string;
-  readonly posted_date: Date;
-  readonly author: { UserID: number };
-}
--- firstsoa/data-layer/dto/create-user.dto.ts
+++
@@ -1,6 +0,0 @@
-export class User {
-  readonly UserID: number;
-  readonly name: string;
-  readonly email: string;
-  readonly password: string;
-}
