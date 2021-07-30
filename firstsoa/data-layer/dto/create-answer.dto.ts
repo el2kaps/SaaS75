804d47dcf0fc5be7ce284e017185b952de8f8776
Eleni Kapsali <63153771+el2kaps@users.noreply.github.com>
2021-07-30 10:55:16 +0300

    Delete firstsoa directory

--- firstsoa/data-layer/dto/create-answer.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Question } from '../entities/question.entity';
-
-export class CreateAnswerDto {
-  readonly text: string;
-  readonly posted_date: Date;
-  readonly question: Question;
-  readonly author: { UserID: number };
-}
