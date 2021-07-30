17d2cd4776b77e9f1ab12e55d064d3bfb438085d
Eleni Kapsali <63153771+el2kaps@users.noreply.github.com>
2021-07-30 10:55:42 +0300

    Delete qa-mng/src directory

--- qa-mng/src/user/dto/create-user.dto.ts
+++
@@ -1,5 +0,0 @@
-import { IsString, IsDate, IsNumber, IsEmail} from 'class-validator';
-export class CreateUserDto {
-  @IsNumber()
-  User_ID: number;
-}
--- qa-mng/src/user/entities/user.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface UserI {
-  User_ID: number;
-}
